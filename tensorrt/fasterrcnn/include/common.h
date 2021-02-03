71c04051b03dfcf7c28be47b60bb52900547240e
zxbgoat <teslasloan@zju.edu.cn>
2021-02-03 10:52:39 +0800

    add some examples for tensorrt

---
+++ tensorrt/fasterrcnn/include/common.h
@@ -0,0 +1,19 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "argsParser.h"
+#include "buffers.h"
+#include "common.h"
+#include "NvCaffeParser.h"
+#include "NvInfer.h"
+
+#include <cuda_runtime_api.h>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using std::string;
+
+#endif //COMMON_H
---
+++ tensorrt/fasterrcnn/include/fasterrcnn.h
@@ -0,0 +1,8 @@
+#ifndef FASTERRCNN_H
+#define FASTERRCNN_H
+
+#include "common.h"
+
+const string gName = "TensorRT.sample_fasterRCNN";
+
+#endif //FASTERRCNN_H
